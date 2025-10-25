46bcb8c10f01c0288b0a4ed95c808a50ef1c51ee
Md. Abdur Rahman <52786537+abdurrahman-eee@users.noreply.github.com>
2025-10-26 01:00:30 +0900

    Refactor routes to use default export

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,8 @@ import type { RouteObject } from "react-router";
 import React from "react";
 import Welcome from "./welcome/welcome";
 
-export const routes: RouteObject[] = [
+const routes: RouteObject[] = [
   { path: "/", element: React.createElement(Welcome) },
 ];
+
+export default routes;
